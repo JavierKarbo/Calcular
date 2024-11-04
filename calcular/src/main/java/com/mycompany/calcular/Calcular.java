51561a90c35a1a241a0f67dc7d311feed174c6ab
2024-2025 DAM-1 <2024-2025 DAM-1@DESKTOP-J71TIL2>
2024-11-04 17:38:58 +0100

    suma

---
+++ calcular/src/main/java/com/mycompany/calcular/Calcular.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcular;
+
+/**
+ *
+ * @author 2024-2025 DAM-1
+ */
+public class Calcular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+//esta es la rama de suma
