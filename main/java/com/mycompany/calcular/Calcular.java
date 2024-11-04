6c7a9add3e81ce28b3099a6f1e388bb4922b0825
JaviKarbo <you@example.com>
2024-11-04 17:07:19 +0100

    Primer intento

---
+++ main/java/com/mycompany/calcular/Calcular.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcular;
+
+/**
+ *
+ * @author 2024-25 DAM1
+ */
+public class Calcular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
